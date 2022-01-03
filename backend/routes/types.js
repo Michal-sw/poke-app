f9d1dee4c031c3dfe424852b38df01e590fb6b72
Michał Świerczewski <michal.swierczewski13@wp.pl>
2022-01-03 19:22:20 +0100

    Api dla zywiolow

--- backend/routes/types.js
+++ backend/routes/types.js
@@ -5,11 +5,18 @@ const Type = require('../models/Type');
 
 router.get('/', async (req, res) => {
   await Type.find()
-  .then(result => console.log(result))
-
-  await Type.find()
-  .then(result => res.json(result))
-  .catch(err => res.status(500).json(err));
+    .then(result => res.json(result))
+    .catch(err => res.status(500).json(err));
 });
 
+router.get('/:name', async (req, res) => {
+  const name = req.params.name;
+  const camelCaseName = name.charAt(0).toUpperCase() + name.slice(1);
+
+  await Type.find({ name: camelCaseName })
+    .then(result => {
+      res.json(result)
+    })
+    .catch(err => res.status(500).json(err));
+})
 module.exports = router;
