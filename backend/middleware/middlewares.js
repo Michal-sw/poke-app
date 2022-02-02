ffd5b79c0923571596a3e59060b92a63559c4335
Michał Świerczewski <michal.swierczewski13@wp.pl>
2022-02-02 18:32:51 +0100

    Trash delete

--- backend/middleware/middlewares.js
+++ backend/middleware/middlewares.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 const authorizeMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
   const token = authHeader;
-  console.log(authHeader, token)
   if (!token) return res.sendStatus(401);
 
   jwt.verify(token, 'S3CR3T', (err, user) => {
