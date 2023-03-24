ebdeaa00ffd027498735c41e39bb137afbf65854
Michał Świerczewski <michal.swierczewski13@wp.pl>
2023-03-24 16:40:05 +0100

    Add message for untrusted certificate

--- frontend/src/ui/pokemons/PokemonList.js
+++ frontend/src/ui/pokemons/PokemonList.js
@@ -44,7 +44,7 @@ const PokemonList = ({ pokemons, loading, query, changeQueryAction, getPokemons 
             <BigText>If you do not see any pokemons, please visit</BigText>
             <BigText>
               <a href="https://130.61.177.18:3000" target='_blank'>https://130.61.177.18:3000</a>
-              and mark the website certificate as trusted.
+              and mark the website certificate as trusted, so the API data can be fetched.
             </BigText>
           </>
     }
